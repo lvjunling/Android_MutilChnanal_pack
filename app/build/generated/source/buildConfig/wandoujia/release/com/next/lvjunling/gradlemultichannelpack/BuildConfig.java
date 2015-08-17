fde8747c93a33e8ecafa819b3fee9815964434bc
ljunling186 <15180111606@163.com>
2015-08-17 16:28:17 +0800

    build

---
+++ app/build/generated/source/buildConfig/wandoujia/release/com/next/lvjunling/gradlemultichannelpack/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.next.lvjunling.gradlemultichannelpack;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.next.lvjunling.gradlemultichannelpack";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "wandoujia";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "8.8.8";
+  // Fields from build type: release
+  public static final String HOST = "https://api.ikaowo.com";
+}
