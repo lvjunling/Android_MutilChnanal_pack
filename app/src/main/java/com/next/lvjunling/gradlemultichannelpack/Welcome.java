311f25b785a98c400ea2aa193add810c7c9c0262
ljunling186 <15180111606@163.com>
2015-08-17 15:29:32 +0800

    init

---
+++ app/src/main/java/com/next/lvjunling/gradlemultichannelpack/Welcome.java
@@ -0,0 +1,18 @@
+package com.next.lvjunling.gradlemultichannelpack;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by lvjunling on 15/8/17.
+ */
+public class Welcome extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+        Log.d("welcome", BuildConfig.HOST);
+    }
+}
